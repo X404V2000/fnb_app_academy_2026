c4c44dd4c4a91f1d562462c079e2eb424a911ef4
Nhlanhla Mawelele <196174071+X404V2000@users.noreply.github.com>
2026-07-24 13:17:40 +0200

    Implement ATM withdrawal simulation logic
    Added functionality to simulate ATM withdrawal with balance checks and error handling.

--- unit_4/atm_sim.py
+++ unit_4/atm_sim.py
@@ -1,3 +1,18 @@
+'''
+The Challenge: “The Smart ATM Withdrawal Simulator”
+Simulate a bank transaction checking if a user has enough money.
+
+1. Set a fixed variable representing a bank balance, for example: balance = 500.
+
+2. Ask the user how much money they want to withdraw. (Remember to cast it to an integer or float!).
+
+3. If the request is less than or equal to the balance, deduct the amount and print:
+“Withdrawal successful! Remaining balance: RX”.
+
+4. But what if they try to withdraw a negative amount or zero? Add an elif statement checking if the request is less than or equal to 0. If so, print: “Invalid amount”. You must withdraw more than “R0”.
+
+5. Otherwise (else), print: “Declined. Insufficient funds”
+'''
 sys_dbase = {"balance": 500}
 
 def view_balance(sys_dbase):
