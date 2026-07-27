98b278d3626ff687e5cff4799a84173c9be76210
Nhlanhla Mawelele <196174071+X404V2000@users.noreply.github.com>
2026-07-27 18:17:31 +0200

    Update phone_dir_search.py

--- unit_6/phone_dir_search.py
+++ unit_6/phone_dir_search.py
@@ -1,3 +1,15 @@
+'''
+The Challenge: “The Phone Directory Search”
+Create a mini data directory using a List and a Dictionary combined.
+
+1. Create a dictionary called contacts where the Keys are friend names and the Values are their phone numbers (keep phone numbers as strings so the leading 0 doesn’t drop off, e.g., “0821112222”). Fill it with 3 people.
+
+2. Ask the user to input the name of the friend they want to look up.
+
+3. Use a conditional check to see if the name matches a key. If it exists, pull out and print their number: “Found! [Name]’s number is [Number]”.
+
+4. Otherwise, print “Contact not found.”
+'''
 contacts = {
 	"Jonathan": "0870634512",
 	"Victor": "0691341245",
